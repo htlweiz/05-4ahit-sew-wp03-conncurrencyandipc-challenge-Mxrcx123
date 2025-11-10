fbdf634ded788a77f7a8a486c329faf6579a30aa
Marco <rechbergermarco74@gmail.com>
2025-11-10 08:04:20 +0100

    first task

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -9,6 +9,15 @@ class Program
     
     public static void Main(string[] args)
     {
+        Thread threada = new Thread(CountUpThreadA);
+        Thread threadb = new Thread(CountDownThreadB);
+
+        threada.Start();
+        threadb.Start();
+
+        threada.Join();
+        threadb.Join();
+
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
         
         
@@ -16,11 +25,18 @@ class Program
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 1; i < 100; i++)
+        {
+            Thread.Sleep(100);
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+        for (int i = 100; i > 1; i--)
+        {
+            Thread.Sleep(100);
+        }
+        
     }
 }
